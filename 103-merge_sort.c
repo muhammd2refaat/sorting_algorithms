880fd86a4c759affe16beda52435f61ed9e68fc9
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-02 06:21:48 +0300

    merge

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,15 +1,48 @@
 #include"sort.h"
 
+
 /**
- * merge_sort - sorts an array of integers in ascending order
- * using the Merge sort algorithm
- * @array: array to sort
- * @size: size of array
+ * merge_sort - function that sorts an array of integers in ascending
+ * order using the Merge sort algorithm
+ * @array: array
+ * @size: size of the array
  */
 void merge_sort(int *array, size_t size)
 {
+	int *left, *right, *result, i, j, k;
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
-	mergesort(array, 0, size - 1, size);
+	left = malloc(sizeof(int) * (size / 2));
+	right = malloc(sizeof(int) * (size - size / 2));
+	result = malloc(sizeof(int) * size);
+	for (i = 0; i < size / 2; i++)
+		left[i] = array[i];
+	for (j = 0; j < size - size / 2; j++)
+		right[j] = array[size / 2 + j];
+	merge_sort(left, size / 2);
+	merge_sort(right, size - size / 2);
+	i = j = 0;
+	k = 0;
+	while (i < size / 2 && j < size - size / 2)
+	{
+		if (left[i] <= right[j])
+		{
+			result[k++] = left[i++];
+		}
+		else
+		{
+			result[k++] = right[j++];
+		}
+	}
+	while (i < size / 2)
+		result[k++] = left[i++];
+	while (j < size - size / 2)
+		result[k++] = right[j++];
+	for (i = 0; i < size; i++)
+		array[i] = result[i];
+	free(left);
+	free(right);
+	free(result);
 }
+
